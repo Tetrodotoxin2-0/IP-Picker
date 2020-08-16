05f9e65385c120538ceb17365d8c840f60eca2c7
TTX <69615429+Tetrodotoxin2-0@users.noreply.github.com>
2020-08-16 10:25:37 +0600

    Add IP-PICKER By Upload

---
+++ IP.py
@@ -0,0 +1,67 @@
+# Coded by Tetrodotoxin <<<< TTX >>>>
+
+# Instagram :~ @Tetrodotoxin2.0
+# Facebook Page :~ https://m.facebook.com/𝚃𝚎𝚊𝚖-𝚃𝚎𝚝𝚛𝚘𝚍𝚘𝚝𝚘𝚡𝚒𝚗-105404181277338
+# Don't Try To Edit
+# By Changing Banner or Text Of the Script you will not Be a Coder
+# If you using my script then give me credit
+
+# Coded by Tetrodotoxin <<<< TTX >>>>
+import socket
+from time import sleep as sleep
+from os import system as sys
+###########################
+def tool():
+  sys("clear")
+  sleep(0.1)
+  print ("\033[0;37m _   _____        _____   _   _____   _   _    _____   _____   ")
+  sleep(0.1)
+  print ("\033[1;31m| | |  _  \      |  _  \ | | /  ___| | | / /  | ____| |  _  \  ")
+  sleep(0.1)
+  print ("\033[1;32m| | | |_| |      | |_| | | | | |     | |/ /   | |__   | |_| |  ")
+  sleep(0.1)
+  print ("\033[1;33m| | |  ___/      |  ___/ | | | |     | |\ \   |  __|  |  _  /  ")
+  sleep(0.1)
+  print ("\033[1;36m| | | |          | |     | | | |___  | | \ \  | |___  | | \ \  ")
+  sleep(0.1)
+  print ("\033[1;94m|_| |_|          |_|     |_| \_____| |_|  \_\ |_____| |_|  \_\ ")
+  sleep(0.2)
+  print ("")
+  sleep(0.1)
+  print ("\033[1;37m  				Version>> 1.∆")
+  sleep(0.1)
+  print ('\033[0;96mCoded By Tetrodotoxin 2.0')
+  sleep(0.1)
+  print ("\033[1;98m")
+  print ("Instagram        :~  @Tetrodotoxin2.0")
+  sleep(0.1)
+  print ("\033[1;98m")
+  print ("Facebook Page  :~  𝚃𝚎𝚊𝚖-𝚃𝚎𝚝𝚛𝚘𝚍𝚘𝚝𝚘𝚡𝚒𝚗")
+  sleep(0.1)
+  print ("\033[1;92m=")
+tool()
+###########################
+print ("=\033[1;96m1\033[1;97m} \033[1;92mUpdate ")
+sleep(0.1)
+print ("=")
+print ("=\033[1;96m2\033[1;97m} \033[1;92mPick IP")
+sleep(0.1)
+print ("=")
+no=input("========[Choose a Number]> ")
+if no =="1":
+  sys("cd && rm -rif IP-Picker && git clone https://github.com/Tetrodotoxin2-0/IP-Picker && cd IP-Picker && bash setup.sh && python3 IP.py")
+###########################
+elif no =="2":
+  tool()
+  print ("=")
+  sleep(0.1)
+  print ("=\033[1;92mNote: Without (HTTP or HTTPS)")
+  sleep(0.1)
+  print ("=")
+  sleep(0.1)
+  host=input("=\033[1;95m$=====\033[1;96m[\033[0;92mType your DNS\033[1;96m]\033[1;93m>>\033[0;96m  ")
+  IP=socket.gethostbyname(host)
+  print ("")
+  sleep(0.2)
+  print (" \033[1;95m==========================", '\n',"",'\n' ,"\033[1;96mHostname>> \033[0;92m", host, '\n',"",'\n',"\033[1;95m==========================" ,'\n',"",'\n', "\033[1;96mTarget IP>> \033[0;92m", IP , '\n',"",'\n',"\033[1;95==========================")
+###########################
